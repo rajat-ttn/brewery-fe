7424f33518976ea71d793d54bdab1d1465be17c4
Pooja-22 <pooja.garg@tothenew.com>
2018-03-07 13:02:56 +0530

    Updated App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,17 @@
-import React, {Component} from 'react';
-import logo from '../assets/logo.svg';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
 import '../css/App.css';
 import TodoContainer from './components/Todo/TodoContainer.js';
 import Dashboard from './components/Dashboard';
-import {Provider} from 'react-redux'; //← Bridge React and Redux
 import store from './store';
 
 class App extends Component {
+    
     render() {
         return (
             <Provider store={store}>
                 <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo"/>
-                        <h1 className="App-title">Welcome to React Todo App</h1>
-                    </header>
-                    <p className="App-intro">
-                        Add Todos!
-                    </p>
                     <Dashboard />
                 </div>
             </Provider>
