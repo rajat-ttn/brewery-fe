d46ca60852b05987ae59f741b39497e25ebd01fa
Poonam <poonam.baveja@tothenew.com>
2018-03-08 17:36:16 +0530

    adding blinking unconditionally

--- src/app/components/BeerComponent/index.js
+++ src/app/components/BeerComponent/index.js
@@ -8,14 +8,14 @@ const BeerComponent = ({ beerContentDetail }) => {
          <div className="col-md-4 commonBoxModel">
               <div className="beerContainer">
                    <h3 className="beertype">{beerType}</h3>
-                   <span className="currentTemp">
+                   <span className="currentTemp danger">
                        <img className="iconStyling" src="../images/temp-icon.png" />
-                       {currentTemperature ? currentTemperature : '--'}
-                    </span>
+                       {currentTemperature ? currentTemperature : '--'} &#8451;
+                   </span>
                    <span className="tmpLabel">Current Temp</span>
                    <span className="rangeTemp">
                        <img className="iconStyling" src="../images/temp-icon.png" />
-                       {`${tempRange[0]} - ${tempRange[1]}`}
+                       {`${tempRange[0]} - ${tempRange[1]}`} &#8451;
                        </span>
                    <span className="tmpLabel">Temp Range</span>
               </div>
