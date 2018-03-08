bec9975e5e2377e615532d614261d12ecf28e1fd
Poonam <poonam.baveja@tothenew.com>
2018-03-08 08:27:13 +0530

    ui changes

--- src/app/components/Dashboard/index.js
+++ src/app/components/Dashboard/index.js
@@ -22,10 +22,10 @@ class Dashboard extends Component {
                 <header><h1>The Brewery - Transport Refrigeration Sensors</h1></header>
                 <div className="row">
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer yellowContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 1" className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 1" className="beerContainerImage"/>
+                            <figcaption className="yellowContainer">
                                 Beer 1<br/>
                                 Current Temp<br/>
                                 Temp Range
@@ -36,10 +36,10 @@ class Dashboard extends Component {
                     </div>
 
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer blueContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 2"  className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 2"  className="beerContainerImage"/>
+                            <figcaption class="blueContainer">
                                 Beer 2<br/>
                                 Current Temp<br/>
                                 Temp Range
@@ -50,10 +50,10 @@ class Dashboard extends Component {
                     </div>
 
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer pinkContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 3"  className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 3"  className="beerContainerImage"/>
+                            <figcaption className="pinkContainer">
                                 Beer 3<br/>
                                 Current Temp<br/>
                                 Temp Range
@@ -64,26 +64,26 @@ class Dashboard extends Component {
                     </div>
 
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer greenContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 4"  className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 4"  className="beerContainerImage"/>
+                            <figcaption class="greenContainer">
                                 Beer 4<br/>
                                 Current Temp<br/>
                                 Temp Range
                             </figcaption>
                             </figure>
-                            <div className="overlay">
-                                 <div className="overlayText">4.5</div>
+                            <div className="overlay commonBoxModel">
+                                 <div className="overlayText">4.5 <p>Below</p></div>
                             </div>
                         </div>
                     </div>
 
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer orangeContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 5"  className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 5"  className="beerContainerImage"/>
+                            <figcaption className="orangeContainer">
                                 Beer 5<br/>
                                 Current Temp<br/>
                                 Temp Range
@@ -94,10 +94,10 @@ class Dashboard extends Component {
                     </div>
 
                     <div className="col-md-4 commonBoxModel">
-                        <div className="beerContainer purpleContainer">
+                        <div className="beerContainer">
                             <figure>
-                             <img src="../images/beer.jpg" alt="Beer 6"  className="beerContainerImage"/>
-                            <figcaption>
+                             <img src="../images/beer.png" alt="Beer 6"  className="beerContainerImage"/>
+                            <figcaption className="purpleContainer">
                                 Beer 6<br/>
                                 Current Temp<br/>
                                 Temp Range
