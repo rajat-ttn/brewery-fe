f76fbcf2a4415815b64a0186901c10ab17d40ea8
Poonam <poonam.baveja@tothenew.com>
2018-03-08 17:19:37 +0530

    layout 2

--- src/app/components/BeerComponent/index.js
+++ src/app/components/BeerComponent/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './style.css';
 
 const BeerComponent = ({ beerContentDetail }) => {
-    const { beerType, tempRange} = beerContentDetail;
+    const { beerType, tempRange, containerId} = beerContentDetail;
     return (
          <div className="col-md-4 commonBoxModel">
               <div className="beerContainer">
-                   <h3 className="beertype">{beerType}</h3>
-                   <span className="currentTemp"><img className="iconStyling" src="../images/temp-icon.png" /> 4.5</span>
-                   <span className="tmpLabel">Current Temp</span>
-                   <span className="rangeTemp"><img className="iconStyling" src="../images/temp-icon.png" /> {`${tempRange[0]} - ${tempRange[1]}`}</span>
-                   <span className="tmpLabel">Temp Range</span>
+                  <h2>Container {containerId}</h2>
+                  <div className="beerInfo">
+                  <strong>Beer Type:</strong> {beerType}
+                  <span className="borderBox"><strong>Temp Range:</strong> {`${tempRange[0]} - ${tempRange[1]}`}</span>
+                  <span className="borderBox"><strong>Current Temp:</strong> 4.5</span>
+                  <span className="status danger"><span className="status-circle high-temp"></span> Above temperature Range</span>
+                  </div>
               </div>
           </div>
     );
