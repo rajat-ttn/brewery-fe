fc155a1d45e1c2396026558c9c5779388b164e41
Poonam <poonam.baveja@tothenew.com>
2018-03-09 17:56:49 +0530

    dummy icon for alarm

--- src/app/components/Common/Header/index.js
+++ src/app/components/Common/Header/index.js
@@ -4,8 +4,9 @@ import './style.css';
 const Header = (props) => {
     return (
         <header>
-             <img src="../images/beer_logo.png" alt="logo" className="logoStyle"/>
+            <img src="../images/beer_logo.png" alt="logo" className="logoStyle"/>
             <h1>The Brewery</h1>
+            <img src="../images/beer_logo.png" alt="alarm" className="bellIcon"/>
         </header>
     );
 }
