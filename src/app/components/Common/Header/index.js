bfca2034d96e6e63e71e852a75f7fd86801ef1dc
Pooja-22 <pooja.garg@tothenew.com>
2018-03-12 13:10:52 +0530

    Changed images path and removed unused files

--- src/app/components/Common/Header/index.js
+++ src/app/components/Common/Header/index.js
@@ -5,10 +5,10 @@ const Header = ({ toggleSound, isMute }) => {
     const imageName = isMute ? 'alarm-mute.png' : 'alarm.png';
     return (
         <header>
-            <img src="../images/beer_logo.png" alt="logo" className="logoStyle"/>
+            <img src="/images/beer_logo.png" alt="logo" className="logoStyle"/>
             <h1>The Brewery</h1>
             <span onClick={toggleSound}>
-            <img src={`../images/${imageName}`} alt="alarm" className="bellIcon"/>
+            <img src={`/images/${imageName}`} alt="alarm" className="bellIcon"/>
             </span>
         </header>
     );
